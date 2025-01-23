9702f014c41a2e48b4db6fb7ed58498c40632672
binbinmods <binbinmods@gmail.com>
2025-01-23 13:50:25 -0500

    Loaded the data from DataText

--- DictionaryToExcel.py
+++ DictionaryToExcel.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import re
+import pickle
+import os
 
 def dicts_to_excel(dict_sheet_pairs, output_file):
    
@@ -16,8 +19,214 @@ def dicts_to_excel(dict_sheet_pairs, output_file):
             
             df.to_excel(writer, sheet_name=sheet_name, index=False)
 
-# Example usage
+def pickle_excel(filename:str):
+    df = pd.read_excel(filename, sheet_name ="eventReply")
+
+    with open("DataTextReference.pkl", "wb") as f:
+        pickle.dump(df, f)    
+
+
+def lookup_events(filename:str, id_list) ->dict:
+    if os.path.exists("DataTextReference.pkl"):
+        with open("DataTextReference.pkl", "rb") as f:
+            df = pickle.load(f)
+    else:
+        df = pd.read_excel(filename, sheet_name ="eventReply")
+        pickle_excel(filename)
+
+    lookup_dict = dict(zip(df['SSLootList'], df['medsEvent']))
+    
+    results = {}
+    for id_value in id_list:
+        results[id_value] = lookup_dict.get(id_value, 'e_sen1_a')
+    
+    return results
+
+def create_lists():
+    regular_drops = """FrozenSewersElves
+            FrozenSewersMix
+            FrozenSewersRatmen
+            Jewelerrings
+            Mansionleft
+            Mansionleftplus
+            Sailor
+            Shady
+            Stolenitems
+            apprentince
+            balanceblack
+            balanceboth
+            balancewhite
+            battlefield
+            bridge
+            chappel
+            crane
+            crocoloot
+            crocosmugglers
+            desertreliquary
+            dreadcosme
+            dreadcuthbert
+            dreadfrancis
+            dreadhoratio
+            dreadjack
+            dreadmimic
+            dreadpurser
+            dreadpurserstash
+            dreadrhodogor
+            eeriechest_a
+            eeriechest_b
+            lavacascade
+            lootedarmory
+            lootsepulchralrare
+            obsidianall
+            obsidiananvil
+            obsidianboots
+            obsidianrings
+            obsidianrods
+            rift
+            riftsen
+            sahtibernardstash
+            sahtidomedesert
+            sahtidomeice
+            sahtidomemain
+            sahtidomemountain
+            sahtidomeswamp
+            sahtipiratearsenal
+            sahtipiratewarehouse
+            sahtiplaguecot
+            sahtisurgeonarsenal
+            sahtisurgeonstash
+            sahtitreasurechamber
+            sahtivalkyriestash
+            thorimsrod
+            treasureaquarfall
+            uprienergy
+            uprimagma
+            voidcraftemerald
+            voidcraftgolden
+            voidcraftobsidian
+            voidcraftpearl
+            voidcraftring
+            voidcraftrodsofblasting
+            voidcraftruby
+            voidcraftsapphire
+            voidcrafttopaz
+            voidtreasurejade
+            watermill"""
+    regular_drops = regular_drops.split()
+    pet_drops = """Jelly
+            betty
+            blobbleed
+            blobcold
+            blobdire
+            blobholy
+            bloblightning
+            blobmetal
+            blobmind
+            blobpoison
+            blobselem
+            blobshadow
+            blobsmyst
+            blobsphys
+            blobwater
+            cuby
+            cubyd
+            daley
+            fenny
+            fishlava
+            inky
+            liante
+            matey
+            mimy
+            obechampy
+            obechompy
+            obechumpy
+            oculy
+            sharpy
+            slimy
+            stormy
+            wolfy"""
+    pet_drops = pet_drops.split()
+    boss_drops = """Basthet
+            Dryad_a
+            Dryad_b
+            Faeborg
+            Hydra
+            Ignidoh
+            Mansionright
+            Mortis
+            Tulah
+            Ylmer
+            belphyor
+            belphyorquest
+            burninghand
+            dreadhalfman
+            elvenarmory
+            elvenarmoryplus
+            goblintown
+            harpychest
+            harpyfight
+            khazakdhum
+            kingrat
+            minotaurcave
+            sahtikraken
+            sahtikrakenmjolmir
+            sahtirustkingtreasure
+            spiderqueen
+            tyrant
+            tyrantbeavers
+            tyrantchampy
+            tyrantchompy
+            tyrantchumpy
+            upripreboss
+            yogger"""
+    boss_drops = boss_drops.split()
+    mythics = """towntier3
+            towntier3_a
+            towntier3_b
+            ruinedplaza
+            ruinedplaza_crit
+            voidasmody
+            voidshop
+            voidtreasure
+            voidtreasurejade
+            voidtsnemo
+            voidtwins
+            wareacc1
+            wareacc2
+            warearm1
+            warearm2
+            warejew1
+            warejew2
+            warenavalea
+            wareweap1
+            wareweap2
+            """
+    mythics = mythics.split()
+    return regular_drops,pet_drops,boss_drops,mythics
+
+
+def convert_event_to_node(event:str) -> str:
+    base:str = event.split("_")
+    try:
+        base = base[1]
+    except:
+        print(f"Error with {event}")
+        return
+    split = re.split('(\d.*)',base)
+    return "_".join(split[:-1])
+
+def get_base_dictionaries():
+    loot_lists = create_lists()
+    ids_to_lookup = loot_lists[1]
+    print(ids_to_lookup)
+
+    events = lookup_events('DataTextReference.xlsx', ids_to_lookup)
+    for id_value, event in events.items():
+        print(f"{{\"{id_value}\", \"{convert_event_to_node(event)}\"}},")
+
 if __name__ == "__main__":
+    get_base_dictionaries()
+
     caravanDict = {}
 
     shopDict = {}
@@ -26,8 +235,6 @@ if __name__ == "__main__":
 
     mythicDict = {}
 
-    
-    # Create list of (dictionary, sheet_name) pairs
     dict_sheet_pairs = [
         (caravanDict, 'Caravan Items'),
         (shopDict, 'Shop Items'),
@@ -35,5 +242,6 @@ if __name__ == "__main__":
         (mythicDict, 'Mythics')
     ]
     
-    # Convert to Excel file
-    dicts_to_excel(dict_sheet_pairs, 'SeedList.xlsx')
+    
+
+    # dicts_to_excel(dict_sheet_pairs, 'SeedList.xlsx')
--- SeedSearcher.cs
+++ SeedSearcher.cs
@@ -76,11 +76,172 @@ namespace SeedSearcher
         public static void LogAllItems()
         {  
             LogInfo("LogAllItems - Start");
-            // LogCaravanItems();
-            // LogGuaranteedShopDropItems();
-            LogShopItems();
+            LogCaravanItems();
+            // LogGuaranteedDropItems();
+            // LogBossDropItems();
+            // LogShopItems();
             // LogMythicItems();
             
+        }
+        internal static void LogPetItems()
+        {
+            Dictionary<string,string> dropNodeMap = new()
+            {
+                {"Jelly", "aquar_41"},
+                {"betty", "sen_39"},
+                {"blobbleed", "sen_29"},
+                {"blobcold", "sewers_13"},
+                {"blobdire", "velka_37"},
+                {"blobholy", "pyr_12"},
+                {"bloblightning", "sahti_30"},
+                {"blobmetal", "velka_38"},
+                {"blobmind", "dread_7"},
+                {"blobpoison", "aquar_44"},
+                {"blobselem", "voidlow_28"},
+                {"blobshadow", "ulmin_59"},
+                {"blobsmyst", "voidlow_28"},
+                {"blobsphys", "voidlow_28"},
+                {"blobwater", "aquar_45"},
+                {"cuby", "pyr_9"},
+                {"cubyd", "pyr_9"},
+                {"daley", "faen_7"},
+                {"fenny", "ulmin_6"},
+                {"fishlava", "forge_8"},
+                {"inky", "aquar_41"},
+                {"liante", "lair_5"},
+                {"matey", "sahti_20"},
+                {"mimy", "dread_10"},
+                {"oculy", "aquar_41"},
+                {"sharpy", "aquar_18"},
+                {"slimy", "lair_7"},
+                {"stormy", "aquar_24"},
+                {"wolfy", "sen_46"},
+
+            };
+            int nSeeds = 300;
+            HandleDropItems(dropNodeMap,nSeeds);
+        }
+
+
+        internal static void LogBossDropItems()
+        {
+            Dictionary<string,string> dropNodeMap = new()
+            {
+                {"Basthet", "pyr_7"},
+                {"Dryad_a", "sen_31"},
+                {"Dryad_b", "sen_32"},
+                {"Faeborg", "faen_38"},
+                {"Hydra", "aquar_35"},
+                {"Ignidoh", "velka_32"},
+                {"Mansionright", "faen_35"},
+                {"Mortis", "ulmin_56"},
+                {"Tulah", "lair_8"},
+                {"Ylmer", "sen_33"},
+                {"belphyor", "secta_5"},
+                {"belphyorquest", "velka_8"},
+                {"burninghand", "velka_25"},
+                {"dreadhalfman", "dread_11"},
+                {"elvenarmory", "faen_29"},
+                {"elvenarmoryplus", "faen_29"},
+                {"goblintown", "velka_6"},
+                {"harpychest", "velka_27"},
+                {"harpyfight", "velka_27"},
+                {"khazakdhum", "velka_29"},
+                {"kingrat", "sewers_8"},
+                {"minotaurcave", "velka_9"},
+                {"sahtikraken", "sahti_65"},
+                {"sahtikrakenmjolmir", "sahti_65"},
+                {"sahtirustkingtreasure", "sahti_62"},
+                {"spiderqueen", "sen_1"},
+                {"tyrant", "faen_25"},
+                {"tyrantbeavers", "faen_25"},
+                {"tyrantchampy", "faen_25"},
+                {"tyrantchompy", "faen_25"},
+                {"tyrantchumpy", "faen_25"},
+                {"upripreboss", "uprising_13"},
+                {"yogger", "sen_27"},
+            };
+            int nSeeds = 300;
+            HandleDropItems(dropNodeMap,nSeeds);
+        }
+
+        internal static void LogGuaranteedDropItems()
+        {
+            Dictionary<string,string> dropNodeMap = new()
+            {
+                {"FrozenSewersElves", "sewers_11"},
+                {"FrozenSewersMix", "sewers_11"},
+                {"FrozenSewersRatmen", "sewers_11"},
+                {"Jewelerrings", "sen_35"},
+                {"Mansionleft", "faen_35"},
+                {"Mansionleftplus", "faen_35"},
+                {"Sailor", "faen_4"},
+                // {"Shady", "sen_1"},
+                {"Stolenitems", "faen_10"},
+                {"apprentince", "sen_24"},
+                {"balanceblack", "ulmin_52"},
+                {"balanceboth", "ulmin_52"},
+                {"balancewhite", "ulmin_52"},
+                {"battlefield", "aquar_6"},
+                {"bridge", "sen_17"},
+                {"chappel", "sen_21"},
+                {"crane", "velka_21"},
+                {"crocoloot", "aquar_7"},
+                {"crocosmugglers", "aquar_48"},
+                {"desertreliquary", "ulmin_58"},
+                {"dreadcosme", "dread_2"},
+                {"dreadcuthbert", "dread_2"},
+                {"dreadfrancis", "dread_2"},
+                {"dreadhoratio", "dread_2"},
+                {"dreadjack", "dread_2"},
+                {"dreadmimic", "dread_10"},
+                // {"dreadpurser", "sen_1"},
+                // {"dreadpurserstash", "sen_1"},
+                {"dreadrhodogor", "dread_2"},
+                {"eeriechest_a", "sen_6"},
+                {"eeriechest_b", "sen_6"},
+                {"lavacascade", "velka_22"},
+                {"lootedarmory", "pyr_10"},
+                {"lootsepulchralrare", "voidlow_18"},
+                {"obsidianall", "forge_3"},
+                {"obsidiananvil", "forge_3"},
+                {"obsidianboots", "forge_3"},
+                {"obsidianrings", "forge_3"},
+                {"obsidianrods", "forge_3"},
+                {"rift", "velka_40"},
+                {"riftsen", "sen_48"},
+                {"sahtibernardstash", "sahti_52"},
+                {"sahtidomedesert", "sahti_49"},
+                {"sahtidomeice", "sahti_49"},
+                {"sahtidomemain", "sahti_49"},
+                {"sahtidomemountain", "sahti_49"},
+                {"sahtidomeswamp", "sahti_49"},
+                {"sahtipiratearsenal", "sahti_25"},
+                {"sahtipiratewarehouse", "sahti_25"},
+                {"sahtiplaguecot", "sahti_11"},
+                {"sahtisurgeonarsenal", "sahti_13"},
+                {"sahtisurgeonstash", "sahti_13"},
+                {"sahtitreasurechamber", "sahti_25"},
+                {"sahtivalkyriestash", "sahti_19"},
+                {"thorimsrod", "forge_8"},
+                {"treasureaquarfall", "aquar_28"},
+                {"uprienergy", "uprising_5"},
+                {"uprimagma", "uprising_4"},
+                {"voidcraftemerald", "voidlow_14"},
+                {"voidcraftgolden", "voidlow_14"},
+                {"voidcraftobsidian", "voidlow_14"},
+                {"voidcraftpearl", "voidlow_14"},
+                {"voidcraftring", "voidlow_14"},
+                // {"voidcraftrodsofblasting", "sen_1"},
+                {"voidcraftruby", "voidlow_14"},
+                {"voidcraftsapphire", "voidlow_14"},
+                {"voidcrafttopaz", "voidlow_14"},
+                {"voidtreasurejade", "voidlow_16"},
+                {"watermill", "sen_15"},
+            };
+            int nSeeds = 300;
+            HandleDropItems(dropNodeMap,nSeeds);
+
         }
 
         internal static void LogShopItems()
@@ -102,10 +263,18 @@ namespace SeedSearcher
                 // {"werewolfstall_b","sen_30"}
             };
 
-            Dictionary<string,List<string>> outputItemMap = [];
             int nSeeds = 300;
-            // LootItem item = lootData.Loot    ItemTable[0];            
-            foreach (string shop in shopNodeMap.Keys)
+            // LootItem item = lootData.Loot    ItemTable[0];
+            HandleDropItems(shopNodeMap,nSeeds);
+            
+
+        }
+
+        internal static void HandleDropItems(Dictionary<string,string> lootNodeMap, int nSeeds = 300)
+        {
+            Dictionary<string,List<string>> outputItemMap = [];
+
+            foreach (string shop in lootNodeMap.Keys)
             {
                 LootData lootData = Globals.Instance.GetLootData(shop);
                 List<string> validItems = [];
@@ -125,15 +294,14 @@ namespace SeedSearcher
                 for (int i = 0; i < nSeeds; i++)
                 {
                 string randomSeed = Functions.RandomStringSafe(7f).ToUpper();
-                string node = shopNodeMap[shop];
-                List<string> itemList = GetItemsFromSeed(_seed:randomSeed,_shop:shop,_node:node, _madness:1);
+                string node = lootNodeMap[shop];
+                List<string> itemList = GetItemsFromSeed(_seed:randomSeed,_shop:shop,_node:node, _madness:8,_corruptorCount:8);
                 // LogDebug($"Shop - {shop} itemList Items - {string.Join(", ",itemList)}");
                 UpdateItemDictForShops(ref outputItemMap,itemList,validItems,randomSeed,shop);
                 }
             }
             
             LogDebug($"Shop Items - {SerializeDictionary(outputItemMap)}");
-
         }
 
         internal static void LogCaravanItems()
